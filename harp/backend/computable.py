c3278dabde5f1ee214059377004e7afc3b047c43
François Steinmetz <fs@hygeos.com>
2025-02-07 15:56:20 +0100

    Fix

--- harp/backend/computable.py
+++ harp/backend/computable.py
@@ -5,7 +5,7 @@ from core.static import interface
 class Computable:
     
     @interface
-    def __init__(self, func: Callable, operands=list[str], keep_operands=False):
+    def __init__(self, func: Callable, operands: list[str], keep_operands=False):
         self.func = func
         self.operands = operands
         self.keep_operands = keep_operands
