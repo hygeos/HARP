247da41b7f03e6c112ccb393601954c2e3063799
Joackim Orcière <joackim.orciere@hygeos.com>
2025-07-21 14:50:28 +0200

    Fixs: HARP_DEBUG var now interpreted as False if set to '0'

--- harp/config.py
+++ harp/config.py
@@ -34,8 +34,7 @@ default_config_dict = dict(
 
 default_config.ingest(default_config_dict)
 
-
-_debug = bool(env.getvar("HARP_DEBUG", False))
+_debug = bool(int(env.getvar("HARP_DEBUG", False)))
 
 if not _debug:
     log.silence(harp, log.lvl.DEBUG)
